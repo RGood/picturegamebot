bfed1ed0b6729a7138356ce619ab51eafffd676f
avinashd <d.nash.avi@gmail.com>
2014-05-10 13:37:45 +0000

    Version bump

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ import warnings
 warnings.filterwarnings("ignore", category=ResourceWarning) 
 
 class PictureGameBot:
-  version = "0.1"
+  version = "0.2"
   user_agent = "/r/PictureGame Bot"
   
   def __init__(bot, gamebot=(None, None), player=(None, None), subreddit="PictureGame"):
